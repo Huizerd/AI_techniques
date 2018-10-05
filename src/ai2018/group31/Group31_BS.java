9aa033ef93b0b9e49a7c7aa554cec1b8f9642187
Ravi Autar <raviautar27@gmail.com>
2018-10-05 18:24:56 +0200

    Add bid offering with gaussian around threshold utility

--- src/ai2018/group31/Group31_BS.java
+++ src/ai2018/group31/Group31_BS.java
@@ -17,10 +17,11 @@ import java.util.*;
 public class Group31_BS extends OfferingStrategy {
 
     List<Double> timeBuckets;
-    List<Double> utilityThresholds;
+    List<Double> utilMeans;
     UtilitySpace utilSpace;
-    Map<Integer, ArrayList<Bid>> bidsBuckets;
+    Map<Double, ArrayList<Bid>> bidBuckets;
     Random random;
+    List<Double> sortedUtilities;
 
 
     public Group31_BS() {
@@ -29,24 +30,24 @@ public class Group31_BS extends OfferingStrategy {
 
     @Override
     public void init(NegotiationSession var1, OpponentModel var2, OMStrategy var3, Map<String, Double> var4) throws Exception {
-        this.timeBuckets = new ArrayList<>(Arrays.asList(0.0, 0.5, 0.8, 0.95, 1.0));
-        this.utilityThresholds = new ArrayList<>(Arrays.asList(1.0, 0.9, 0.8, 0.7, 0.55));
+        this.timeBuckets = new ArrayList<>(Arrays.asList(0.0, 0.5, 0.95, 1.0));
+        this.utilMeans = new ArrayList<>(Arrays.asList(1.0, 0.9, 0.8, 0.6));
         this.random = new Random();
         this.negotiationSession = var1;
         this.opponentModel = var2;
         this.omStrategy = var3;
         this.utilSpace = this.negotiationSession.getUtilitySpace();
-        this.bidsBuckets = new HashMap<>();
+        this.bidBuckets = new HashMap<>();
         this.initBidSpace();
-        for (Bid bid : this.bidsBuckets.get(0)) {
-            System.out.println(bid + ": " + this.utilSpace.getUtility(bid));
+        sortedUtilities = new ArrayList<>(this.bidBuckets.keySet());
+        Collections.sort(this.sortedUtilities);
+        for (double d : sortedUtilities) {
+            System.out.println(d);
         }
+
     }
 
     public void initBidSpace() {
-        for (int i = 0; i < this.utilityThresholds.size() - 1; i++) {
-            this.bidsBuckets.put(i, new ArrayList<>());
-        }
         BidIterator bidIterator = new BidIterator(this.utilSpace.getDomain());
         if (!bidIterator.hasNext()) {
             throw new IllegalStateException("The domain does not contain any bids!");
@@ -54,12 +55,10 @@ public class Group31_BS extends OfferingStrategy {
             while (bidIterator.hasNext()) {
                 Bid bid = bidIterator.next();
                 double bidUtil = this.utilSpace.getUtility(bid);
-
-                for (int i = 0; i < this.utilityThresholds.size() - 1; i++) {
-                    if (bidUtil <= this.utilityThresholds.get(i) && bidUtil > this.utilityThresholds.get(i + 1)) {
-                        this.bidsBuckets.get(i).add(bid);
-                    }
+                if (!this.bidBuckets.containsKey(bidUtil)) {
+                    this.bidBuckets.put(bidUtil, new ArrayList<>());
                 }
+                this.bidBuckets.get(bidUtil).add(bid);
             }
         }
     }
@@ -69,15 +68,30 @@ public class Group31_BS extends OfferingStrategy {
         double time = this.negotiationSession.getTime();
         for (int i = 0; i < this.timeBuckets.size() - 1; i++) {
             if (time >= this.timeBuckets.get(i) && time < this.timeBuckets.get(i + 1)) {
-                List<Bid> possibleBids = this.bidsBuckets.get(i);
-                int index = random.nextInt(possibleBids.size());
-                return new BidDetails(possibleBids.get(index), this.utilSpace.getUtility(possibleBids.get(index)), time);
+                double gauss = getGaussWithBounds(this.utilMeans.get(i), this.utilMeans.get(i+1), 1.0);
+                // Get closest utility value.
+                List<Double> sortedUtilcopy = new ArrayList<>(this.sortedUtilities);
+                sortedUtilcopy.replaceAll(x -> Math.abs(x - gauss));
+                int minIndex = sortedUtilcopy.indexOf(Collections.min(sortedUtilcopy));
+                List<Bid> gaussBidList = this.bidBuckets.get(this.sortedUtilities.get(minIndex));
+                Bid bid = gaussBidList.get(random.nextInt(gaussBidList.size()));
+                return new BidDetails(bid, this.utilSpace.getUtility(bid), time);
             }
         }
-//        return this.negotiationSession.getMinBidinDomain();
+        System.out.println("Return NUll");
         return null;
-
     }
+
+    private double getGaussWithBounds(double mean, double lowerBound, double upperBound) {
+        // Generate Gaussian value with utility mean and std of 0.1.
+        while (true) {
+            double gauss = random.nextGaussian() * 0.1 + mean;
+            if (lowerBound <= gauss && gauss <= upperBound) {
+                return gauss;
+            }
+        }
+    }
+
 
     public BidDetails determineOpeningBid() {
         return this.determineNextBid();
