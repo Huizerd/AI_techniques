649bd8ee9dafc807845aad82b99e5888bc2821a7
Ravi Autar <raviautar27@gmail.com>
2018-10-21 15:55:19 +0200

    Add window for HardHeaded OM

--- src/ai2018/group31/Group31_OM.java
+++ src/ai2018/group31/Group31_OM.java
@@ -20,25 +20,26 @@ import java.util.*;
 // (powered by Fernflower decompiler)
 //
 
-        import genius.core.Bid;
-        import genius.core.bidding.BidDetails;
-        import genius.core.boaframework.BOAparameter;
-        import genius.core.boaframework.NegotiationSession;
-        import genius.core.boaframework.OpponentModel;
-        import genius.core.issue.Issue;
-        import genius.core.issue.IssueDiscrete;
-        import genius.core.issue.Objective;
-        import genius.core.issue.Value;
-        import genius.core.issue.ValueDiscrete;
-        import genius.core.utility.AdditiveUtilitySpace;
-        import genius.core.utility.Evaluator;
-        import genius.core.utility.EvaluatorDiscrete;
-        import java.util.HashMap;
-        import java.util.HashSet;
-        import java.util.Iterator;
-        import java.util.Map;
-        import java.util.Set;
-        import java.util.Map.Entry;
+import genius.core.Bid;
+import genius.core.bidding.BidDetails;
+import genius.core.boaframework.BOAparameter;
+import genius.core.boaframework.NegotiationSession;
+import genius.core.boaframework.OpponentModel;
+import genius.core.issue.Issue;
+import genius.core.issue.IssueDiscrete;
+import genius.core.issue.Objective;
+import genius.core.issue.Value;
+import genius.core.issue.ValueDiscrete;
+import genius.core.utility.AdditiveUtilitySpace;
+import genius.core.utility.Evaluator;
+import genius.core.utility.EvaluatorDiscrete;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
 
 public class Group31_OM extends OpponentModel {
@@ -55,74 +56,77 @@ public class Group31_OM extends OpponentModel {
     public void init(NegotiationSession var1, Map<String, Double> var2) {
         this.negotiationSession = var1;
         if (var2 != null && var2.get("l") != null) {
-            this.learnCoef = (Double)var2.get("l");
+            this.learnCoef = var2.get("l");
         } else {
             this.learnCoef = 0.2D;
         }
 
         this.learnValueAddition = 1;
-        this.opponentUtilitySpace = (AdditiveUtilitySpace)var1.getUtilitySpace().copy();
+        this.opponentUtilitySpace = (AdditiveUtilitySpace) var1.getUtilitySpace().copy();
         this.amountOfIssues = this.opponentUtilitySpace.getDomain().getIssues().size();
-        this.goldenValue = this.learnCoef / (double)this.amountOfIssues;
+        this.goldenValue = this.learnCoef / (double) this.amountOfIssues;
         this.initializeModel();
+
 //        INITIALIZATION OF TIME WINDOW SIZE
         this.windowSize = 5;
     }
 
     public void updateModel(Bid var1, double var2) {
-        if (this.negotiationSession.getOpponentBidHistory().size() >= 2) {
-            int var4 = 0;
+        int h = this.negotiationSession.getOpponentBidHistory().size();
+        if (h < 2) return;
 
-            LinkedList<BidDetails> prevBid = new LinkedList<>();
+        int commonOccurences = 0;
+        int wz = h < this.windowSize ? h : this.windowSize;
 
-            for(int i=1; i<=this.windowSize; i++){
-                prevBid.add((BidDetails)this.negotiationSession.getOpponentBidHistory()
-                        .getHistory().get(this.negotiationSession.getOpponentBidHistory().size() - i));
-            }
+        BidDetails currBid = this.negotiationSession.getOpponentBidHistory().getHistory().get(this.negotiationSession.getOpponentBidHistory().size() - 1);
+        Map<Integer, Integer> differenceLookup = new HashMap();
 
-            HashMap var7 = this.determineDifference(prevBid, currBid);
-            Iterator var8 = var7.keySet().iterator();
-
-            while(var8.hasNext()) {
-                Integer var9 = (Integer)var8.next();
-                if ((Integer)var7.get(var9) == 0) {
-                    ++var4;
-                }
-            }
-
-            double var20 = 1.0D + this.goldenValue * (double)var4;
-            double var10 = 1.0D - (double)this.amountOfIssues * this.goldenValue / var20;
-
-            Iterator var12;
-            Objective var14;
-            double var17;
-            for(var12 = var7.keySet().iterator(); var12.hasNext(); this.opponentUtilitySpace.setWeight(var14, var17)) {
-                Integer var13 = (Integer)var12.next();
-                var14 = this.opponentUtilitySpace.getDomain().getObjectivesRoot().getObjective(var13);
-                double var15 = this.opponentUtilitySpace.getWeight(var13);
-                if ((Integer)var7.get(var13) == 0 && var15 < var10) {
-                    var17 = (var15 + this.goldenValue) / var20;
-                } else {
-                    var17 = var15 / var20;
-                }
-            }
-
-            try {
-                var12 = this.opponentUtilitySpace.getEvaluators().iterator();
-
-                while(var12.hasNext()) {
-                    Map.Entry var21 = (Map.Entry)var12.next();
-                    EvaluatorDiscrete var22 = (EvaluatorDiscrete)var21.getValue();
-                    IssueDiscrete var23 = (IssueDiscrete)var21.getKey();
-                    ValueDiscrete var16 = (ValueDiscrete)currBid.getBid().getValue(var23.getNumber());
-                    Integer var24 = var22.getEvaluationNotNormalized(var16);
-                    var22.setEvaluation(var16, this.learnValueAddition + var24);
-                }
-            } catch (Exception var19) {
-                var19.printStackTrace();
-            }
-
+        for (int i = 2; i <= wz; i++) {
+            BidDetails prevBid = this.negotiationSession.getOpponentBidHistory().getHistory().get(this.negotiationSession.getOpponentBidHistory().size() - i);
+            differenceLookup = determineDifference(prevBid, currBid, new HashMap<>(differenceLookup));
         }
+
+        Iterator iter = differenceLookup.keySet().iterator();
+
+        while (iter.hasNext()) {
+            Integer issueNo = (Integer) iter.next();
+            if (differenceLookup.get(issueNo) == 0) {
+                ++commonOccurences;
+            }
+        }
+
+        double var20 = 1.0D + this.goldenValue * (double) commonOccurences;
+        double var10 = 1.0D - (double) this.amountOfIssues * this.goldenValue / var20;
+
+        Iterator iter2;
+        Objective objective;
+        double var17;
+        for (iter2 = differenceLookup.keySet().iterator(); iter2.hasNext(); this.opponentUtilitySpace.setWeight(objective, var17)) {
+            Integer currentDiff = (Integer) iter2.next();
+            objective = this.opponentUtilitySpace.getDomain().getObjectivesRoot().getObjective(currentDiff);
+            double var15 = this.opponentUtilitySpace.getWeight(currentDiff);
+            if (differenceLookup.get(currentDiff) == 0 && var15 < var10) {
+                var17 = (var15 + this.goldenValue) / var20;
+            } else {
+                var17 = var15 / var20;
+            }
+        }
+
+        try {
+            iter2 = this.opponentUtilitySpace.getEvaluators().iterator();
+
+            while (iter2.hasNext()) {
+                Map.Entry var21 = (Map.Entry) iter2.next();
+                EvaluatorDiscrete var22 = (EvaluatorDiscrete) var21.getValue();
+                IssueDiscrete var23 = (IssueDiscrete) var21.getKey();
+                ValueDiscrete var16 = (ValueDiscrete) currBid.getBid().getValue(var23.getNumber());
+                Integer var24 = var22.getEvaluationNotNormalized(var16);
+                var22.setEvaluation(var16, this.learnValueAddition + var24);
+            }
+        } catch (Exception var19) {
+            var19.printStackTrace();
+        }
+
     }
 
     public double getBidEvaluation(Bid var1) {
@@ -138,7 +142,7 @@ public class Group31_OM extends OpponentModel {
     }
 
     public String getName() {
-        return "HardHeaded Frequency Model";
+        return "2018 - Group 31 Model";
     }
 
     public Set<BOAparameter> getParameterSpec() {
@@ -148,20 +152,20 @@ public class Group31_OM extends OpponentModel {
     }
 
     private void initializeModel() {
-        double var1 = 1.0D / (double)this.amountOfIssues;
+        double var1 = 1.0D / (double) this.amountOfIssues;
         Iterator var3 = this.opponentUtilitySpace.getEvaluators().iterator();
 
-        while(var3.hasNext()) {
-            Map.Entry var4 = (Map.Entry)var3.next();
-            this.opponentUtilitySpace.unlock((Objective)var4.getKey());
-            ((Evaluator)var4.getValue()).setWeight(var1);
+        while (var3.hasNext()) {
+            Map.Entry var4 = (Map.Entry) var3.next();
+            this.opponentUtilitySpace.unlock((Objective) var4.getKey());
+            ((Evaluator) var4.getValue()).setWeight(var1);
 
             try {
-                Iterator var5 = ((IssueDiscrete)var4.getKey()).getValues().iterator();
+                Iterator var5 = ((IssueDiscrete) var4.getKey()).getValues().iterator();
 
-                while(var5.hasNext()) {
-                    ValueDiscrete var6 = (ValueDiscrete)var5.next();
-                    ((EvaluatorDiscrete)var4.getValue()).setEvaluation(var6, 1);
+                while (var5.hasNext()) {
+                    ValueDiscrete var6 = (ValueDiscrete) var5.next();
+                    ((EvaluatorDiscrete) var4.getValue()).setEvaluation(var6, 1);
                 }
             } catch (Exception var7) {
                 var7.printStackTrace();
@@ -170,22 +174,21 @@ public class Group31_OM extends OpponentModel {
 
     }
 
-    private HashMap<Integer, Integer> determineDifference(BidDetails var1, BidDetails var2) {
-        HashMap var3 = new HashMap();
-
+    private HashMap<Integer, Integer> determineDifference(BidDetails prevBid, BidDetails currBid, Map<Integer, Integer> diffLookup) {
         try {
-            Iterator var4 = this.opponentUtilitySpace.getDomain().getIssues().iterator();
+            Iterator iter = this.opponentUtilitySpace.getDomain().getIssues().iterator();
 
-            while(var4.hasNext()) {
-                Issue var5 = (Issue)var4.next();
-                Value var6 = var1.getBid().getValue(var5.getNumber());
-                Value var7 = var2.getBid().getValue(var5.getNumber());
-                var3.put(var5.getNumber(), var6.equals(var7) ? 0 : 1);
+            while (iter.hasNext()) {
+                Issue issue = (Issue) iter.next();
+                Value prevBidVal = prevBid.getBid().getValue(issue.getNumber());
+                Value currBidVal = currBid.getBid().getValue(issue.getNumber());
+                diffLookup.put(issue.getNumber(), (diffLookup.get(issue.getNumber()) == 0 ||
+                        prevBidVal.equals(currBidVal)) ? 0 : 1);
             }
-        } catch (Exception var8) {
-            var8.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        return var3;
+        return (HashMap<Integer, Integer>) diffLookup;
     }
 }
